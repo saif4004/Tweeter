a04e5a7bf59ce16812d4898c6d0b2c8812908fdd
Saif Al-saedi <saif4004@gmail.com>
2024-10-09 14:40:58 -0700

    Implemented an event listener for when the user clicks the tweet button. added AJAX to POST to the server and GET back the infromation from the server. Lastly implemented LoadTweets function it calls renderTweets and pass it the data it gest from the server

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,33 +5,39 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 $(document).ready(function() {
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    },
-
-  ]
   
+  const handleUserTweet = (event) => {
+    event.preventDefault();
+    const userTweet = $("#user-tweet").serialize();
 
+    $.ajax({
+      method: 'POST',
+      url: '/tweets',
+      data: userTweet,
+      success: (response) => {
+        console.log('Success: ',response);
+      },
+      error: (error) => {
+        console.error('Error',error);
+      }
+    })
+  };
+  const loadTweets = () => {
+    $.ajax ({
+      method: 'GET',
+      url: '/tweets',
+      dataType: 'JSON',
+      success: (data) => {
+        renderTweets(data);
+        
+      },
+      error: (error) => {
+        console.log('this request failed and this was the error', error);
+      },
+    });
+  }
+  loadTweets();
+  $('#user-tweet').submit(handleUserTweet);
   
 
 
@@ -51,7 +57,11 @@ $(document).ready(function() {
       const $faHeart = $("<i class='fa-solid fa-heart'></i>");
       const $faFlag = $("<i class='fa-solid fa-flag'></i>");
       const $faRetweet = $("<i class='fa-solid fa-retweet'></i>");
-      const $span = $("<span></span>").text(tweet.created_at);
+
+      //Post Time
+      const createdAtTime = timeago.format(tweet.created_at);
+      const $span = $("<span></span>").text(createdAtTime);
+
 
       $contentHeaderDiv.append($img).append($h2);
 
@@ -71,5 +81,4 @@ $(document).ready(function() {
       $('.tweets-container').append(createTweetElement($tweet));
     }
   }
-  renderTweets(data);
 });
