ec72ff6178e3b2527a965c6e3f357f049d2c848b
Saif Al-saedi <saif4004@gmail.com>
2024-10-09 13:35:59 -0700

    Added the renderTweets function.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,20 +1,38 @@
+
 /*
  * Client-side JS logic goes here
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 $(document).ready(function() {
-  const tweetData = {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
         "handle": "@SirIsaac"
       },
-    "content": {
+      "content": {
         "text": "If I have seen further it is by standing on the shoulders of giants"
       },
-    "created_at": 1461116232227
-  }
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    },
+
+  ]
+  
+
+  
 
 
   const createTweetElement = (tweet) => {
@@ -27,7 +45,7 @@ $(document).ready(function() {
       const $h3 = $("<h3></h3>").text(tweet.user.handle);
       const $p = $("<p></p>").text(tweet.content.text);
 
-      //Footer with the icons 
+      //Footer with the icons and span
       const $footer = $("<footer></footer>");
       const $iconDiv = $("<div></div>");
       const $faHeart = $("<i class='fa-solid fa-heart'></i>");
@@ -48,7 +66,10 @@ $(document).ready(function() {
       return $article;
   }
 
-  const $tweet = createTweetElement(tweetData);
-  console.log($tweet); // to see what it looks like
-  $('.tweet-container').append($tweet);
+  const renderTweets = function(tweets) {
+    for (const $tweet of tweets) {
+      $('.tweets-container').append(createTweetElement($tweet));
+    }
+  }
+  renderTweets(data);
 });
